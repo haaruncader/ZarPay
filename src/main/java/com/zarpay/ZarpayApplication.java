de33460b307efb3bff78cf400aee20bcfe3bb908
haaruncader <cdrhaa001@myuct.ac.za>
2026-01-24 11:52:43 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/zarpay/ZarpayApplication.java
@@ -0,0 +1,13 @@
+package com.zarpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zarpay/ZarpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
